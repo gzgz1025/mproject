172d3a5f0584cba0dd32a880f3dc00a4ea672905
1024262077@qq.com <pzl688pzl>
2020-10-23 16:41:12 +0800

    新增websocket 聊天功能

--- src/main/java/com/example/mproject/shiro/controller/LoginController.java
+++ src/main/java/com/example/mproject/shiro/controller/LoginController.java
@@ -1,3 +1,4 @@
+/*
 package com.example.mproject.shiro.controller;
 
 import com.example.mproject.shiro.bean.User;
@@ -14,7 +15,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "logincontroller")
 @Slf4j
 public class LoginController {
 
@@ -65,3 +66,4 @@ public class LoginController {
         return "add success!";
     }
 }
+*/

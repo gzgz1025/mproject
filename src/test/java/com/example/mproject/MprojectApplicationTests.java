4819c6df6daf566f9ce7c463f9b4c96a41f56e5d
1024262077@qq.com <pzl688pzl>
2020-10-27 15:16:47 +0800

    小修改

--- src/test/java/com/example/mproject/MprojectApplicationTests.java
+++ src/test/java/com/example/mproject/MprojectApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.mproject;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
 class MprojectApplicationTests {
 
     @Test
     void contextLoads() {
     }
+    @Test
+    public void test(){
+        Integer number = new Integer(8);
+        for(int i=1;i<=number;i++) {
+            for(int m=0;m<number-i;m++) {
+                System.out.print(" ");
+            }
+            if(i==1) {
+                System.out.println("*");
+            }
+            else if(i==number) {
+                for(int n=1;n<=2*number-1;n++) {
+                    System.out.print("*");
+                }
+                System.out.println("\n");
+            }
+            else {
+                System.out.print("*");
+                for(int p=1;p<=2*i-3;p++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+        }
 
+    }
 }
--- src/test/java/com/example/mproject/emailtest/SendMailTest.java
+++ src/test/java/com/example/mproject/emailtest/SendMailTest.java
@@ -23,4 +23,45 @@ public class SendMailTest {
     public void sendAttachmentMail() {
         mailService.sendAttachmentMail(toEmail,"这是带附件的邮件","有附件","D:\\笔记整理\\使用spring boot实现发送邮件功能.md");
     }
+
+    @Test
+    public void test(){
+        Integer number = new Integer(8);
+        for(int i=1;i<=number;i++) {
+            for(int m=0;m<number-i;m++) {
+                System.out.print(" ");
+            }
+            if(i==1) {
+                System.out.println("*");
+            }
+            else if(i==number) {
+                for(int n=1;n<=2*number-1;n++) {
+                    System.out.print("*");
+                }
+                System.out.println("\n");
+            }
+            else {
+                System.out.print("*");
+                for(int p=1;p<=2*i-3;p++) {
+                    System.out.print(" ");
+                }
+                System.out.println("*");
+            }
+        }
+    }
+    @Test
+    public void testxx(){
+        float x, y;
+        for (y = (float)1.5; y > -1.5; y -= 0.1) {
+            for (x = (float)-1.5; x < 1.5; x += 0.05) {
+                float a = x * x + y * y - 1;
+                if ((a * a * a - x * x * y * y * y) <= 0.0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
